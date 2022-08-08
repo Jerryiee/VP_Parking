c44c641c54cf09cd24cbbaeb743a42c3d06da5c3
Jerryiee <jaroslavvorcak1@gmail.com>
2022-08-08 12:56:52 +0200

    Updated main, added buttons, function call

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import functions_display
 import parking
 
+while True:
+    now = datetime.datetime.now()
+    if((now.hour == 18) and (dim == False)):
+        diming(3)
+        dim = True
+    elif((dim == True) and (now.hour == 6)) :
+        diming(4)
+        dim = False

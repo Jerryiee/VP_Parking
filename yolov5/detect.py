3265f94d1a0a248a50990487fe1a4a0d40974a80
Just in <39840269+Jerryiee@users.noreply.github.com>
2022-09-07 15:42:59 +0200

    counting in/out,  ID remaining

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -32,6 +32,7 @@ from pathlib import Path
 
 import torch
 import torch.backends.cudnn as cudnn
+from yolov5.utils.tracker import EuclideanDistTracker
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
@@ -50,7 +51,9 @@ cars = [2, 7]
 cars_in = 0
 cars_out = 0
 im_centroids = []
+detections = []
 last_centroids =()
+tracker = EuclideanDistTracker()
 
 
 @smart_inference_mode()
@@ -169,16 +172,16 @@ def run(
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
                     if save_img or save_crop or view_img:  # Add bbox to image
-                        global im_centroids
+                        global im_centroids, detections
                         c = int(cls)  # integer class
                         label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
                         annotator.box_label(xyxy, label, color=colors(c, True))
                         x1 = int(xyxy[0] + (int(xyxy[2]-xyxy[0])/2))
                         y1 = int(xyxy[1] + (int(xyxy[3]-xyxy[1])/2))
                         centroid = (x1 , y1)
-                        cv2.circle(im0, centroid, radius=5, color=(0,255,0), thickness=2)
-                        #print(centroid)
+                        cv2.circle(im0, centroid, radius=5, color=(0,255,0), thickness=2) 
                         im_centroids.append(centroid)
+                        detections.append(centroid)
                     if save_crop:
                         save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
@@ -186,31 +189,40 @@ def run(
             global last_centroids, cars_in, cars_out
             im0 = annotator.result()
             if view_img:
-                start_point =(0, h-120)
-                end_point= (w,h-120)
-                #print(im_centroids)
+                start_point =(0, h-220)
+                end_point= (w,h-220)
+                (sx1, sy1) = start_point
+                (ex1, ey1) = end_point
+                boxes_ids = tracker.update(detections)
+                for box_id in boxes_ids:
+                    cx, cy ,id = box_id
+                    cv2.putText(im0, str(id),(cx,cy-15),  cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+
                 if(len(last_centroids) != 0):
-                    #print(last_centroids)
-                    print(last_centroids)
-                    print(im_centroids)
                     for cntx, cnty in last_centroids:
                         cv2.circle(im0, (cntx, cnty), radius=5, color=(255,0,255), thickness=1)
-                        #print(cntx, cnty)
+
                     for i in range(len(im_centroids)):
                         nearest = min(last_centroids, key=lambda x: distanceCalculate(x, im_centroids[i]))
-                        print(im_centroids[i])
-                        print(nearest)
                         dis = distanceCalculate(nearest, im_centroids[i])
-                        if(dis<30):
+                        if(dis<50):
+                            (nx,ny) = nearest
+                            (ix, iy) = im_centroids[i]
+                            m = (ey1-sy1)/(ex1-sx1)
+                            line_last= m * (nx-sx1) + sy1
+                            line_centroid= m * (ix-sx1) + sy1
                             line_params = calcParams(nearest, im_centroids[i])
                             intercept_line_params = calcParams((start_point), (end_point))
                             cv2.line(im0, nearest, im_centroids[i] ,(0,0,255),1)
 
-                            if(areLinesIntersecting(intercept_line_params,line_params,nearest, im_centroids[i], im0)):
-                                cars_in += 1
+                            if(areLinesIntersecting(intercept_line_params,line_params ,nearest, im_centroids[i], im0)):
+                                if(line_centroid < iy):
+                                    cars_in += 1
+                                elif(line_last < ny):
+                                    cars_out += 1
                     
-                cv2.putText(im0, str(cars_in), (50,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1, cv2.LINE_AA)# show count
-                cv2.putText(im0, str(cars_out), (50,100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,0), 1, cv2.LINE_AA)# show count
+                cv2.putText(im0, str(cars_in), (50,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1, cv2.LINE_AA)# show count
+                cv2.putText(im0, str(cars_out), (50,100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1, cv2.LINE_AA)# show count
                 cv2.line(im0,(start_point),(end_point),(200,200,0),2) #intercepting line
                 if platform.system() == 'Linux' and p not in windows:
                     windows.append(p)
@@ -220,10 +232,11 @@ def run(
 
                 last_centroids = im_centroids
                 im_centroids = []
+                detections = []
                 #cv2.putText(im0, str(cars_in), (50,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1, cv2.LINE_AA)# show count
 
                 cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
+                cv2.waitKey(0)  # 1 millisecond
 
             # Save results (image with detections)
             if save_img:

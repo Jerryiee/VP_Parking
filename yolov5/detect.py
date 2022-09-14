1bd1d5dfc4b84d9a6091bf73d55bd3c8a613f69e
Just in <39840269+Jerryiee@users.noreply.github.com>
2022-09-14 14:24:14 +0200

    add counting as function, more lines

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -51,6 +51,7 @@ cars_in = 0
 cars_out = 0
 im_centroids = []
 detections = []
+detection_lines = []
 data_count = []
 last_centroids =()
 tracker = EuclideanDistTracker()
@@ -182,77 +183,29 @@ def run(
                         x1 = int(xyxy[0] + (int(xyxy[2]-xyxy[0])/2))
                         y1 = int(xyxy[1] + (int(xyxy[3]-xyxy[1])/2))
                         centroid = (x1 , y1)
-                        cv2.circle(im0, centroid, radius=3, color=(0,255,0), thickness=-1) 
+                        cv2.circle(im0, centroid, radius=3, color=colors(c, True), thickness=-1) 
                         im_centroids.append(centroid)
                     if save_crop:
                         save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
             # Stream results
-            global last_centroids, cars_in, cars_out, data_count, start_point, end_point
             im0 = annotator.result()
             if view_img:
-                 #line points
-                #start_point =(120, h-120)
-                #end_point= (w-120,h-120)
-                if(start_point is not None and end_point is not None):
+                
+                counting(im0) #counting function
 
-                    (sx1, sy1) = start_point
-                    (ex1, ey1) = end_point
-                    line = (start_point, end_point)
-                    detection = tracker.update(im_centroids)
-
-                    if(len(last_centroids) != 0):
-                    #draw centroid from last positions
-                    #for cntx, cnty in last_centroids: 
-                        #cv2.circle(im0, (cntx, cnty), radius=5, color=(255,0,255), thickness=-1)
-                    #find nearest last centroid to every current centorid 
-                        for i in range(len(im_centroids)):
-                            (cx, cy ,id) = detection[i]
-                            cv2.putText(im0, str(id),(cx,cy-15),  cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 2)
-                            nearest = min(last_centroids, key=lambda x: distanceCalculate(x, im_centroids[i]))
-                            dis = distanceCalculate(nearest, im_centroids[i])
-                            cv2.circle(im0, nearest, radius=3, color=(255,0,255), thickness=-1) #draw centroid from last positions, only nearest
-                            if(dis<40):
-                                (nx,ny) = nearest
-                                (ix, iy) = im_centroids[i]
-                                m = (ey1-sy1)/(ex1-sx1)
-                                line_last= m * (nx-sx1) + sy1
-                                line_centroid= m * (ix-sx1) + sy1
-                                line_points = (nearest, im_centroids[i])
-                                cv2.line(im0, nearest, im_centroids[i] ,(0,0,255),1)
-                                #chcek if lines are intersecting
-                                if(intersects(line, line_points)):
-                                    #chcek if from what side cross the line and if is id on data
-                                    if(line_centroid < iy and id not in data_count):
-                                        data_count.append(id)
-                                        #print(id)
-                                        cars_in += 1
-                                    elif(line_last < ny and id not in data_count):
-                                        data_count.append(id)
-                                        cars_out += 1
-                                        #print(id)
-                    
-                    cv2.putText(im0, str(cars_in), (50,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1, cv2.LINE_AA)# show count
-                    cv2.putText(im0, str(cars_out), (50,100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,255,0), 1, cv2.LINE_AA)# show count
-                    cv2.line(im0,(start_point),(end_point),(0,0,255),2) #intercepting line
                 if platform.system() == 'Linux' and p not in windows:
                     windows.append(p)
                     cv2.namedWindow(str(p), cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)  # allow window resize (Linux)
                     cv2.resizeWindow(str(p), im0.shape[1], im0.shape[0])
-                
 
-                last_centroids = im_centroids
-                im_centroids = []
-                detections = []
-                #cv2.putText(im0, str(cars_in), (50,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,255), 1, cv2.LINE_AA)# show count
                 k = cv2.waitKey(1)
                 cv2.namedWindow("video")
-                #if k == 27 or k == 32 or k == 13:
-                    #break
+                if k == 27 or k == 32 or k == 13:
+                    break
                 cv2.imshow("video", im0)
                 cv2.setMouseCallback("video", drawLine)
-                
-                #cv2.waitKey(1)  # 1 millisecond
+
 
             # Save results (image with detections)
             if save_img:
@@ -319,6 +272,64 @@ def parse_opt():
     print_args(vars(opt))
     return opt
 
+def counting(im0):
+    global last_centroids, cars_in, cars_out, data_count, start_point, end_point, detection_lines, im_centroids, detections
+    #line points
+    #start_point =(120, h-120)
+                #end_point= (w-120,h-120)
+
+    if(detection_lines != 0):
+
+        for b in range(len(detection_lines)):
+
+            sx1, sy1, ex1, ey1 = detection_lines[b]
+            start_point = (sx1, sy1)
+            end_point = (ex1, ey1)
+            line = (start_point, end_point)
+            detection = tracker.update(im_centroids)
+
+            if(len(last_centroids) != 0):
+
+            #draw centroid from last positions
+            #for cntx, cnty in last_centroids: 
+            #cv2.circle(im0, (cntx, cnty), radius=5, color=(255,0,255), thickness=-1)
+
+                #find nearest last centroid to every current centorid 
+                for i in range(len(im_centroids)):
+                    (cx, cy ,id) = detection[i]
+                    cv2.putText(im0, str(id),(cx,cy-15),  cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 2)
+                    nearest = min(last_centroids, key=lambda x: distanceCalculate(x, im_centroids[i]))
+                    dis = distanceCalculate(nearest, im_centroids[i])
+
+                                #cv2.circle(im0, nearest, radius=3, color=(255,0,255), thickness=-1) #draw centroid from last positions, only nearest
+
+                    if(dis<40):
+                        (nx, ny) = nearest
+                        (ix, iy) = im_centroids[i]
+                        m = (ey1-sy1)/(ex1-sx1)
+                        line_last= m * (nx-sx1) + sy1
+                        line_centroid= m * (ix-sx1) + sy1
+                        line_points = (nearest, im_centroids[i])
+                        #cv2.line(im0, nearest, im_centroids[i] ,(0,0,255),1) line betwen centroid and last near centroid
+                        #chcek if lines are intersecting
+                        if(intersects(line, line_points)):
+                            #chcek if from what side cross the line and if is id on data
+                            if(line_centroid < iy and id not in data_count):
+                                data_count.append(id)
+                                #print(id)
+                                cars_in += 1
+                            elif(line_last < ny and id not in data_count):
+                                data_count.append(id)
+                                cars_out += 1
+                                #print(id)
+                        
+            cv2.putText(im0, str(cars_in), (50,50), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,255), 1, cv2.LINE_AA)# show count
+            cv2.putText(im0, str(cars_out), (50,100), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,255,0), 1, cv2.LINE_AA)# show count
+            cv2.line(im0,(start_point),(end_point),(0,0,255),2) #intercepting line
+    last_centroids = im_centroids
+    im_centroids = []
+    detections = []
+
 
 def distanceCalculate(p1, p2):
     """p1 and p2 in format (x1,y1) and (x2,y2) tuples"""
@@ -327,16 +338,14 @@ def distanceCalculate(p1, p2):
 
 def drawLine(event, x, y, flags, param):
     # Mouse event handlers for drawing lines
-    global x1, y1, start_point, end_point, drawing
+    global x1, y1, drawing, detection_lines
     if event == cv2.EVENT_LBUTTONDOWN:
         if drawing == False:  # Start drawing a line
             x1, y1 = x, y
             drawing = True
         else:  # Stop drawing a line
             x2, y2 = x, y
-            start_point = (x1, y1)
-            end_point = (x2, y2)
-            #detection_lines.append([sx1, sy1, ex1, ey1])
+            detection_lines.append([x1, y1, x2, y2])
             drawing = False
 
 def on_segment(p, q, r):
